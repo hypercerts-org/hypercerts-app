606f0ab9dc48a755da0d24f7b811d6ab8fc5adf1
jipstavenuiter <jipstavenuiter@gmail.com>
2024-09-12 15:35:05 +0200

    feat: fix styling of form

--- app/collections/create/page.tsx
+++ app/collections/create/page.tsx
@@ -2,7 +2,7 @@ import { CollectionCreateForm } from "@/components/collections/collection-create
 
 const CreateCollectionPage = async () => {
   return (
-    <main className="flex flex-col p-8 md:p-24 pb-24 space-y-4">
+    <main className="flex flex-col p-8 md:p-24 pb-24 space-y-4 container max-w-6xl">
       <div className="w-full">
         <h1 className="font-serif text-3xl lg:text-5xl tracking-tight w-full mb-3">
           New collection
