0ddf35646e0eb2a97b7e7506ca87eb77b354d7f0
jipstavenuiter <jipstavenuiter@gmail.com>
2024-09-22 12:59:50 +0200

    feat: add pagination to collections overview

--- app/profile/[address]/collections-tab-content.tsx
+++ app/profile/[address]/collections-tab-content.tsx
@@ -4,9 +4,28 @@ import { HyperboardRow } from "@/components/hyperboard/hyperboard-row";
 import { Suspense } from "react";
 import { defaultDescription } from "@/app/profile/[address]/tabs";
 import { CreateCollectionButton } from "@/components/collections/buttons";
+import { COLLECTIONS_PER_PAGE } from "@/configs/ui";
+import CollectionPagination from "@/components/collections/collection-pagination";
 
-const CollectionsTabContentInner = async ({ address }: { address: string }) => {
-  const hyperboards = await getCollectionsByAdminAddress(address.toLowerCase());
+const CollectionsTabContentInner = async ({
+  address,
+  searchParams,
+}: {
+  address: string;
+  searchParams: Record<string, string>;
+}) => {
+  const currentPage = Number(searchParams?.p) || 1;
+  const result = await getCollectionsByAdminAddress({
+    adminAddress: address.toLowerCase(),
+    first: COLLECTIONS_PER_PAGE,
+    offset: COLLECTIONS_PER_PAGE * (currentPage - 1),
+  });
+
+  if (!result) {
+    return null;
+  }
+
+  const { hyperboards, count } = result;
 
   if (!hyperboards?.length) {
     return (
@@ -42,14 +61,27 @@ const CollectionsTabContentInner = async ({ address }: { address: string }) => {
           />
         ))}
       </div>
+
+      <div className="mt-5">
+        <CollectionPagination collectionsCount={count || 0} />
+      </div>
     </div>
   );
 };
 
-const CollectionsTabContent = ({ address }: { address: string }) => {
+const CollectionsTabContent = ({
+  address,
+  searchParams,
+}: {
+  address: string;
+  searchParams: Record<string, string>;
+}) => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <CollectionsTabContentInner address={address} />
+      <CollectionsTabContentInner
+        address={address}
+        searchParams={searchParams}
+      />
     </Suspense>
   );
 };
--- collections/getCollectionsByAdminAddress.ts
+++ collections/getCollectionsByAdminAddress.ts
@@ -5,11 +5,17 @@ import { graphql, readFragment } from "@/lib/graphql";
 import { HYPERCERTS_API_URL_GRAPH } from "@/configs/hypercerts";
 import { HyperboardFragment } from "./hyperboardFragment";
 import request from "graphql-request";
+import { COLLECTIONS_PER_PAGE } from "@/configs/ui";
 
 const query = graphql(
   `
-    query Collection($admin_address: String!) {
-      hyperboards(where: { admin_id: { eq: $admin_address } }) {
+    query Collection($admin_address: String!, $first: Int, $offset: Int) {
+      hyperboards(
+        where: { admin_id: { eq: $admin_address } }
+        first: $first
+        offset: $offset
+      ) {
+        count
         data {
           ...HyperboardFragment
         }
@@ -19,15 +25,29 @@ const query = graphql(
   [HyperboardFragment],
 );
 
-export async function getCollectionsByAdminAddress(adminAddress: string) {
+export async function getCollectionsByAdminAddress({
+  adminAddress,
+  first = COLLECTIONS_PER_PAGE,
+  offset = 0,
+}: {
+  adminAddress: string;
+  first?: number;
+  offset?: number;
+}) {
   const res = await request(HYPERCERTS_API_URL_GRAPH, query, {
     admin_address: adminAddress,
+    first,
+    offset,
   });
 
   const collectionsFragment = res.hyperboards?.data;
+  const count = res.hyperboards?.count;
   if (!collectionsFragment) {
     return undefined;
   }
 
-  return readFragment(HyperboardFragment, collectionsFragment);
+  return {
+    count,
+    hyperboards: readFragment(HyperboardFragment, collectionsFragment),
+  };
 }
