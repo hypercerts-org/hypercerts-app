0e16ce8c47cb748c7d7dc6bbeec579f8dc51f1ca
jipstavenuiter <jipstavenuiter@gmail.com>
2024-10-01 10:36:41 +0200

    chore: trigger deploy

--- components/settings/settings-form.tsx
+++ components/settings/settings-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import z from "zod";
-import { useForm } from "react-hook-form";
+import { useForm, useWatch } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useAccount, useEnsAvatar, useEnsName } from "wagmi";
 import {
@@ -56,14 +56,17 @@ export const SettingsForm = () => {
     isPendingGetEnsName ||
     isPendingGetEnsAvatar;
 
-  const form = useForm({
+  const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: defaultValues,
-    disabled: isPending,
-    mode: "onBlur",
+    mode: "all",
+    reValidateMode: "onChange",
   });
 
-  const { displayName, avatar } = form.watch();
+  const [displayName, avatar] = useWatch({
+    control: form.control,
+    name: ["displayName", "avatar"],
+  });
 
   const [updatedUserName, setUpdatedUserName] = useState(false);
   const [updatedUserNameEns, setUpdatedUserNameEns] = useState(false);
@@ -102,19 +105,6 @@ export const SettingsForm = () => {
   const submitDisabled =
     form.formState.isSubmitting || !form.formState.isValid || isPending;
 
-  console.log({
-    displayName,
-    avatar,
-    isPending,
-    isPendingGetUser,
-    isPendingUpdateUser,
-    isPendingGetEnsName,
-    isPendingGetEnsAvatar,
-    updatedUserName,
-    form,
-    test: "test",
-  });
-
   const showAvatar =
     avatar && !form.formState.isValidating && !form.formState.errors.avatar;
 
@@ -128,29 +118,35 @@ export const SettingsForm = () => {
           <FormField
             control={form.control}
             name="displayName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Display name</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-                <FormDescription>Max. 30 characters</FormDescription>
-              </FormItem>
-            )}
+            render={({ field }) => {
+              // console.log(field);
+              return (
+                <FormItem>
+                  <FormLabel>Display name</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isPending} />
+                  </FormControl>
+                  <FormMessage />
+                  <FormDescription>Max. 30 characters</FormDescription>
+                </FormItem>
+              );
+            }}
           />
           <FormField
             control={form.control}
             name="avatar"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Image</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+            render={({ field }) => {
+              // console.log(field);
+              return (
+                <FormItem>
+                  <FormLabel>Image</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isPending} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
           />
 
           {showAvatar && (
