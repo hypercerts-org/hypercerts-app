c86fe2ac169b746d6c7bfea0fe154b2bbc38e2b5
bitbeckers <code@bitbeckers.com>
2025-02-11 10:35:49 +0100

    chore(cleanup): cleanup unused hypercerts hooks and typos
    Removes unused hooks and services from the hypercerts folder

--- components/allowlist/create-allowlist-dialog.tsx
+++ components/allowlist/create-allowlist-dialog.tsx
@@ -16,7 +16,7 @@ import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { errorHasMessage } from "@/lib/errorHasMessage";
 import { toast } from "@/components/ui/use-toast";
-import { useValidateAllowlist } from "@/hypercerts/hooks/useCreateAllowLists";
+import { useValidateAllowList } from "@/hypercerts/hooks/useValidateAllowList";
 import { AllowlistEntry } from "@hypercerts-org/sdk";
 
 import { DEFAULT_NUM_FRACTIONS } from "@/configs/hypercerts";
@@ -41,7 +41,7 @@ export default function Component({
     isPending,
     error: createAllowListError,
     reset,
-  } = useValidateAllowlist();
+  } = useValidateAllowList();
   const [allowList, setAllowList] = useState<AllowListItem[]>([
     {
       address: "",
